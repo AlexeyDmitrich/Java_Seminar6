043b6b11cbd1f19a8b1f37ad680cbdd9c6dab653
/dev/null <a.d.kamenev@yandex.ru>
2023-03-20 23:20:23 +0300

    Homework started.

---
+++ src/Homework/Factory.java
@@ -0,0 +1,46 @@
+package Homework;
+
+import java.util.Random;
+
+public class Factory {
+    Random rnd = new Random();
+    private int order;
+
+    public Factory(int order) {
+        this.order = order;
+    }
+
+    public Laptop[] fillStock (){
+        String[] model = {"Acuc", "Levono", "Intesit", "Sansumg", "Presstiger", "Aker", "Motonola"};
+        String modelName = modelNameGen();
+        int[] ram= {2, 4, 8, 16, 32, 64, 128, 256};
+        int[] memory= {64, 128, 256, 512, 1024, 2048, 4096};
+        String[] os= {"Linux", "MS Dos", "Windows 10", "Android", "FreeBSD"};
+        String[] color= {"Черный", "Серый", "Белый"};
+        Laptop[] stock = new Laptop[this.order];
+        for (int i = 0; i < stock.length; i++) {
+            stock[i] = new Laptop(
+                    model[rnd.nextInt(model.length)],
+                    modelName,
+                    ram[rnd.nextInt(ram.length)],
+                    memory[rnd.nextInt(memory.length)],
+                    os[rnd.nextInt(os.length)],
+                    color[rnd.nextInt(color.length)]);
+        }
+        return stock;
+    }
+
+    public String modelNameGen (){
+        StringBuilder uin = new StringBuilder();
+        String[] prefix = {"a","b","c","d","e","f","g","i","k","j","l","m","n","o","p","q","r","s","t","u"};
+        Integer body = (rnd.nextInt(3000 - 80) + 80);
+        uin.append(prefix[rnd.nextInt(prefix.length)].toUpperCase());
+        uin.append(prefix[rnd.nextInt(prefix.length)].toUpperCase());
+        uin.append(prefix[rnd.nextInt(prefix.length)].toUpperCase());
+        uin.append("-");
+        uin.append(body.toString());
+        uin.append(prefix[rnd.nextInt(prefix.length)]);
+        uin.append(prefix[rnd.nextInt(prefix.length)]);
+        return uin.toString();
+    }
+}
---
+++ src/Homework/Laptop.java
@@ -0,0 +1,19 @@
+package Homework;
+
+public class Laptop {
+    private String model;
+    private String modelName;
+    private int ram;
+    private int memory;
+    private String os;
+    private String color;
+
+    public Laptop(String model, String modelName, int ram, int memory, String os, String color) {
+        this.model = model;
+        this.modelName = modelName;
+        this.ram = ram;
+        this.memory = memory;
+        this.os = os;
+        this.color = color;
+    }
+}
