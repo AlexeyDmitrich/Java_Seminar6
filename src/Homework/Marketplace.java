b40edb24d56609586f2aa884743e25ec257f4ee5
Alexey <a.d.kamenev@yandex.ru>
2023-03-21 08:06:45 +0300

    Starded hw

--- src/Homework/Marketplace.java
+++ src/Homework/Marketplace.java
@@ -1,5 +1,8 @@
 package Homework;
 
 public class Marketplace {
-
+    public static void main(String[] args) {
+        Factory order = new Factory(100);
+        Laptop[] stock = order.fillStock();
+    }
 }
